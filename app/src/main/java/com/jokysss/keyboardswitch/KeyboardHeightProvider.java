9370b24a42cc055211b8b0adfeec3328892d7944
jokyxray@gmail.com <jokyxray@gmail.com>
2018-07-17 19:02:15 +0800

    Initial commit

---
+++ app/src/main/java/com/jokysss/keyboardswitch/KeyboardHeightProvider.java
@@ -0,0 +1,77 @@
+package com.jokysss.keyboardswitch;
+
+import android.app.Activity;
+import android.graphics.Point;
+import android.graphics.Rect;
+import android.graphics.drawable.ColorDrawable;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewTreeObserver.OnGlobalLayoutListener;
+import android.view.WindowManager.LayoutParams;
+import android.widget.PopupWindow;
+
+/**
+ * The keyboard height provider, this class uses a PopupWindow
+ * to calculate the window height when the floating keyboard is opened and closed.
+ */
+public class KeyboardHeightProvider extends PopupWindow {
+
+    private KeyboardHeightObserver observer;
+    private View popupView;
+    private View parentView;
+    private Point screenSize = new Point();
+    private Rect rect = new Rect();
+
+    public KeyboardHeightProvider(Activity activity) {
+        super(activity);
+        this.popupView = LayoutInflater.from(activity).inflate(R.layout.popupwindow, null, false);
+        setContentView(popupView);
+        setSoftInputMode(LayoutParams.SOFT_INPUT_ADJUST_RESIZE | LayoutParams.SOFT_INPUT_STATE_ALWAYS_VISIBLE);
+        setInputMethodMode(PopupWindow.INPUT_METHOD_NEEDED);
+        parentView = activity.findViewById(android.R.id.content);
+        setWidth(0);
+        setHeight(LayoutParams.MATCH_PARENT);
+        activity.getWindowManager().getDefaultDisplay().getSize(screenSize);
+        popupView.getViewTreeObserver().addOnGlobalLayoutListener(new OnGlobalLayoutListener() {
+            @Override
+            public void onGlobalLayout() {
+                if (popupView != null) {
+                    handleOnGlobalLayout();
+                }
+            }
+        });
+    }
+
+    public void start() {
+        if (!isShowing() && parentView.getWindowToken() != null) {
+            setBackgroundDrawable(new ColorDrawable(0));
+            showAtLocation(parentView, Gravity.NO_GRAVITY, 0, 0);
+        }
+    }
+
+    public void close() {
+        this.observer = null;
+        dismiss();
+    }
+
+    public void setKeyboardHeightObserver(KeyboardHeightObserver observer) {
+        this.observer = observer;
+    }
+
+    private void handleOnGlobalLayout() {
+        popupView.getWindowVisibleDisplayFrame(rect);
+        int keyboardHeight = screenSize.y - rect.bottom;
+        notifyKeyboardHeightChanged(keyboardHeight);
+    }
+
+    private void notifyKeyboardHeightChanged(int height) {
+        if (observer != null) {
+            observer.onKeyboardHeightChanged(height);
+        }
+    }
+
+    public interface KeyboardHeightObserver {
+        void onKeyboardHeightChanged(int height);
+    }
+}
---
+++ app/src/main/java/com/jokysss/keyboardswitch/MainActivity.java
@@ -0,0 +1,85 @@
+package com.jokysss.keyboardswitch;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+
+
+public class MainActivity extends AppCompatActivity implements KeyboardHeightProvider.KeyboardHeightObserver {
+    LinearLayout bottom;
+    LinearLayout content;
+    LinearLayout inputBg;
+    ImageView mSwitch;
+    private int keyboradH = -1;
+    private KeyboardHeightProvider keyboardHeightProvider;
+    private InputMethodManager imm;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottom = findViewById(R.id.bottom);
+        content = findViewById(R.id.content);
+        inputBg = findViewById(R.id.inputbg);
+        mSwitch = findViewById(R.id.iv_switch);
+        imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
+        keyboardHeightProvider = new KeyboardHeightProvider(this);
+        mSwitch.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (bottom.getVisibility() == View.VISIBLE) {
+                    bottom.setVisibility(View.GONE);
+                } else {
+                    imm.hideSoftInputFromWindow(bottom.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+                    bottom.setVisibility(View.VISIBLE);
+                }
+            }
+        });
+        mSwitch.post(new Runnable() {
+            public void run() {
+                keyboardHeightProvider.start();
+            }
+        });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        inputBg.setVisibility(View.GONE);
+        keyboardHeightProvider.setKeyboardHeightObserver(null);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        keyboardHeightProvider.setKeyboardHeightObserver(this);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        keyboardHeightProvider.close();
+    }
+
+    @Override
+    public void onKeyboardHeightChanged(int height) {
+        Log.i("Main", "onKeyboardHeightChanged in pixels: " + height);
+        if (keyboradH == -1 && height > 0) {
+            keyboradH = height;
+            RelativeLayout.LayoutParams params = (RelativeLayout.LayoutParams) inputBg.getLayoutParams();
+            params.height = keyboradH;
+        }
+
+        if (height > 0) {
+            bottom.setVisibility(View.GONE);
+            inputBg.setVisibility(View.VISIBLE);
+        } else {
+            inputBg.setVisibility(View.GONE);
+        }
+    }
+}
