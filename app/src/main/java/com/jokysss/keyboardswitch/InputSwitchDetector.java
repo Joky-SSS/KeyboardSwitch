be3eb7fef23cb9a8e9b0fdca49cbfe4f4f01cbc0
jokyxray@gmail.com <jokyxray@gmail.com>
2018-09-04 19:02:25 +0800

    fix bug

--- app/src/main/java/com/jokysss/keyboardswitch/InputSwitchDetector.java
+++ app/src/main/java/com/jokysss/keyboardswitch/InputSwitchDetector.java
@@ -4,6 +4,7 @@ import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.res.Resources;
 import android.graphics.Rect;
 import android.os.Build;
 import android.util.DisplayMetrics;
@@ -16,9 +17,10 @@ import android.widget.LinearLayout;
 import java.util.HashMap;
 import java.util.Map;
 
+
 public class InputSwitchDetector {
 
-    private static final String SHARE_PREFERENCE_NAME = "input_switch_detector";
+    private static final String SHARE_PREFERENCE_NAME = "emotioninputdetector";
     private static final String SHARE_PREFERENCE_TAG = "soft_input_height";
 
     private Activity mActivity;
@@ -27,7 +29,7 @@ public class InputSwitchDetector {
     private EditText mEditText;
     private View mContentView;
     private Map<View, View> viewPair = new HashMap<>();
-
+    private static int navigationBarH = -1;
     private InputSwitchDetector() {}
 
     public static InputSwitchDetector with(Activity activity) {
@@ -119,6 +121,7 @@ public class InputSwitchDetector {
             unlockContentHeightDelayed(false);
         } else if (inputShow && isBottomShown()) {
             inputH = sp.getInt(SHARE_PREFERENCE_TAG, 0);
+            inputH = 0;
             params.weight = 0F;
             if (inputH == 0) {
                 params.height = contentH;
@@ -150,6 +153,7 @@ public class InputSwitchDetector {
     private int getSupportSoftInputHeight() {
         Rect r = new Rect();
         mActivity.getWindow().getDecorView().getWindowVisibleDisplayFrame(r);
+        Rect o = new Rect();
         int height = mActivity.getWindow().getDecorView().getRootView().getBottom();
         int softInputHeight = height - r.bottom;
         //        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
@@ -160,9 +164,24 @@ public class InputSwitchDetector {
             softInputHeight = 0;
         }
         if (softInputHeight > 0) {
+            int naviBarHeight = getNavigationBarHeight();
+            if (softInputHeight == naviBarHeight)
+                softInputHeight = 0;
+        }
+        if (softInputHeight > 0) {
             sp.edit().putInt(SHARE_PREFERENCE_TAG, softInputHeight).apply();
         }
         return softInputHeight;
+    }
+
+    public int getNavigationBarHeight() {
+        if (navigationBarH == -1) {
+            Resources resources = mActivity.getResources();
+            int resourceId = resources.getIdentifier("navigation_bar_height", "dimen", "android");
+            //获取NavigationBar的高度
+            navigationBarH = resources.getDimensionPixelSize(resourceId);
+        }
+        return navigationBarH;
     }
 
     @TargetApi(Build.VERSION_CODES.JELLY_BEAN_MR1)
